66d7917f68063fa04a4a21b16836c082d59dfa24
erwanmtd <100424606+erwanmtd@users.noreply.github.com>
2022-06-07 09:37:27 +0200

    algo2

---
+++ algo2.java
@@ -0,0 +1,8 @@
+public class Remove {
+ 
+    public static void main(String[] args) {
+        String string = "Cou cou";
+        System.out.println("Avant : \"" + string + "\"");
+        System.out.println("Remplacer: \"" + string.replace(" ", "") + "\"");
+    }
+}
